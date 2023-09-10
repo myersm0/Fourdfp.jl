4425217b75bf03e6810dcecbc4581d4b4d25e8da
Michael Myers <michael.myers@wustl.edu>
2023-09-10 15:05:02 -0500

    Added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using Fourdfp
 using Test
 
+data_dir = joinpath(dirname(@__FILE__), "data")
+mask_path = joinpath(data_dir, "glm_atlas_mask_333.4dfp.img")
+func_path = joinpath(data_dir, "functional_image_333.4dfp.img")
+
 @testset "Fourdfp.jl" begin
-    # Write your tests here.
+	@test read_4dfp(mask_path) isa Array{Float32, 4}
+	@test read_4dfp(func_path) isa Array{Float32, 4}
+
+	mask = read_4dfp(mask_path)
+	func = read_4dfp(func_path)
+
+	@test Fourdfp.get_dims(mask_path) == [48, 64, 48, 1]
+	@test Fourdfp.get_dims(func_path) == [48, 64, 48, 5]
+
+	mask_inds = findall(mask .!= 0)
+	@test length(mask_inds) == 65549
+	@test all([all(func[:, :, :, f][mask_inds] .!= 0) for f in 1:5])
+	@test sum(func .!= 0) == 5 * length(mask_inds)
 end
+
