8c55ca589fe487994fa803b942b279dad7d3f131
Michael Myers <michael.myers@wustl.edu>
2023-09-20 18:15:39 -0500

    Added endianness handling via user argument to load()

--- src/Fourdfp.jl
+++ src/Fourdfp.jl
@@ -3,6 +3,9 @@ module Fourdfp
 
 using Chain
 
+@enum ByteOrder LittleEndian BigEndian
+export LittleEndian, BigEndian
+
 "Trim off .4dfp.* file extension, if any, from a filename"
 get_imgroot(filename::String) = replace(filename, r".4dfp.(ifh|hdr|img(.rec)?)$" => "")
 
@@ -17,7 +20,7 @@ function get_dims(filename::String)::Vector{Int}
 	end
 end
 
-function get_endianness(filename::String)::String
+function get_endianness(filename::String)::ByteOrder
 	endianness =
 		@chain begin
 			"$(Fourdfp.get_imgroot(filename)).4dfp.ifh"
@@ -25,25 +28,27 @@ function get_endianness(filename::String)::String
 			filter(x -> !isnothing(match(r"^imagedata byte order", x)), _)
 			replace.(_, r".*:= (\w+)" => s"\1")
 		end
-	if length(endianness) == 1
-		@assert endianness[1] in ("littleendian", "bigendian")
-		return endianness[1]
+	if length(endianness) == 1 && endianness[1] in ("littleendian", "bigendian")
+		return endianness[1] == "littleendian" ? LittleEndian : BigEndian
 	else
-		@assert length(endianness) == 0
-		return "littleendian"
+		error("Byte order (endianness) could not be parsed from ifh file for $filename")
 	end
 end
 
 """
-    load(filename; dtype = Float32)
+	 load(filename; byte_order)
 
-A very basic 4dfp-reading function; doesn't yet take endianness into account!
+A simple 4dfp-reading function.
 
 Filename sould be the path of a 4dfp file, optionally omitting the file extension.
+
+Byte order should be one of LittleEndian or BigEndian; or omit this argument
+and the function will attempt to parse the byte order from the .4dfp.ifh file.
 """
-function load(filename::String; dtype::Type = Float32)::Array{dtype, 4}
+function load(filename::String; byte_order::Union{Nothing, ByteOrder} = nothing)
 	imgroot = get_imgroot(filename)
 	dims = get_dims(imgroot)
+	dtype = Float32
 	dsize = sizeof(dtype)
 	nvoxels = prod(dims[1:3])
 	nframes = length(dims) < 4 ? 1 : dims[4]
@@ -53,8 +58,10 @@ function load(filename::String; dtype::Type = Float32)::Array{dtype, 4}
 	open("$imgroot.4dfp.img", "r") do fid
 		readbytes!(fid, temp, bytes_to_read)
 	end
-	endianness = get_endianness(filename)
-	byte_order_fn = endianness == "littleendian" ? ltoh : ntoh
+	if isnothing(byte_order) 
+		byte_order = get_endianness(filename)
+	end
+	byte_order_fn = byte_order == LittleEndian ? ltoh : ntoh
 	@chain reinterpret(dtype, temp) byte_order_fn.(_) reshape(_, dims...)
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,18 @@ mask_path = joinpath(data_dir, "glm_atlas_mask_333.4dfp.img")
 func_path = joinpath(data_dir, "functional_image_333.4dfp.img")
 
 @testset "Fourdfp.jl" begin
-	@test Fourdfp.load(mask_path) isa Array{Float32, 4}
-	@test Fourdfp.load(func_path) isa Array{Float32, 4}
+	mask = Fourdfp.load(mask_path; byte_order = BigEndian)
+	@test mask isa Array{Float32, 4}
+	func = Fourdfp.load(func_path; byte_order = LittleEndian)
+	@test func isa Array{Float32, 4}
 
-	mask = Fourdfp.load(mask_path)
-	func = Fourdfp.load(func_path)
+	# should be able to load the func file correctly without specifying endianness
+	# because endianness is specified in its .ifh file
+	func_duplicate = Fourdfp.load(func_path)
+	@test func == func_duplicate
+
+	# the mask file however does not specify endianness in the ifh so this should error:
+	@test_throws ErrorException Fourdfp.load(mask_path)
 
 	@test Fourdfp.get_dims(mask_path) == [48, 64, 48, 1]
 	@test Fourdfp.get_dims(func_path) == [48, 64, 48, 5]
@@ -18,6 +25,8 @@ func_path = joinpath(data_dir, "functional_image_333.4dfp.img")
 	mask_inds = findall(mask .!= 0)
 	@test length(mask_inds) == 65549
 	@test all([all(func[:, :, :, f][mask_inds] .!= 0) for f in 1:5])
+	@test maximum(abs.(func)) < 5
+	@test all(mask[mask_inds] .== 1)
 	@test sum(func .!= 0) == 5 * length(mask_inds)
 end
 
